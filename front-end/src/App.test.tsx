6c1b1331976e3d046d653c75956eb6f7e5896812
Diego Escalante Maldonado <diego.escalante@encora.com>
2025-04-13 17:05:46 -0600

    Update App.test.tsx
    Added a test that checks if the main components renders in the main app

--- front-end/src/App.test.tsx
+++ front-end/src/App.test.tsx
@@ -1,9 +1,12 @@
+import {render, screen, fireEvent} from '@testing-library/react';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Checking if all the components are being rendered inside App", () => {
+   test("Renders the components for the app", ()=>{
+        render(<App />);
+        expect(screen.getByTestId("SearchFilter")).toBeInTheDocument();
+        expect(screen.getByTestId("NewToDo")).toBeInTheDocument();
+        expect(screen.getByTestId("TaskTable")).toBeInTheDocument();
+   });
 });
